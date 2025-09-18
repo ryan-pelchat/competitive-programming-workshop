ca6aa10de7bdf9ef07cfc1fb602f0c0e3466554e
ryan-pelchat <gitryanpelchat@gmail.com>
2025-09-18 09:34:17 -0400

    Solved restaurant.py

--- Week_02/Hour_2_Ryan/tempCodeRunnerFile.py
+++ Week_02/Hour_2_Ryan/tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
-                    # combinedToggle = False
+            # # if stack1 is empty then transfer to stack1
+            # if not stack1:
+            #     output.append(f"MOVE 2->1 {stack2}")
+            #     stack1 += stack2
+            #     stack2 = 0
