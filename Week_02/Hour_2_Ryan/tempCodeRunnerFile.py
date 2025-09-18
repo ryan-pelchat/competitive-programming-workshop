8cf5e65aaddabb4ea58218ec38fb3447ba3099a0
ryan-pelchat <gitryanpelchat@gmail.com>
2025-09-18 09:37:21 -0400

    added empty file problems to finish

--- Week_02/Hour_2_Ryan/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-            # # if stack1 is empty then transfer to stack1
-            # if not stack1:
-            #     output.append(f"MOVE 2->1 {stack2}")
-            #     stack1 += stack2
-            #     stack2 = 0
